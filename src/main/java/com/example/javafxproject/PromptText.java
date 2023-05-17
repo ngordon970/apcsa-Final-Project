77b281c18e8108f8f3c409396eab234c89123f43
ngordon970 <133019853+ngordon970@users.noreply.github.com>
2023-05-17 09:41:22 -0400

    Update PromptText.java

--- src/main/java/com/example/javafxproject/PromptText.java
+++ src/main/java/com/example/javafxproject/PromptText.java
@@ -3,6 +3,7 @@ import java.io.FileReader;
 
 
 public class PromptText {
+    
     public static String generateEasyPrompt() {
         StringBuilder easyPrompt = new StringBuilder();
         try {
@@ -20,5 +21,43 @@ public class PromptText {
             e.printStackTrace();
         }
         return easyPrompt.toString();
+    }
+    
+        public static String generateMedPrompt() {
+        StringBuilder medPrompt = new StringBuilder();
+        try {
+            FileReader reader = new FileReader("C:\\Users\\Noah\\IdeaProjects\\javaFXPROJECT\\src\\main\\java\\com\\example\\javafxproject\\medPrompt.txt");
+            int data = reader.read();
+            while(data != -1) {
+                medPrompt.append((char)data);
+                data = reader.read();
+            }
+            reader.close();
+            int randomLoc = (int)(Math.random() * medPrompt.length() - 150);
+            medPrompt = new StringBuilder(medPrompt.substring(randomLoc));
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+        return medPrompt.toString();
+    }
+    
+       public static String generateExtremePrompt() {
+        StringBuilder extremePrompt = new StringBuilder();
+        try {
+            FileReader reader = new FileReader("C:\\Users\\Noah\\IdeaProjects\\javaFXPROJECT\\src\\main\\java\\com\\example\\javafxproject\\extremePrompt.txt");
+            int data = reader.read();
+            while(data != -1) {
+                extremePrompt.append((char)data);
+                data = reader.read();
+            }
+            reader.close();
+            int randomLoc = (int)(Math.random() * extremePrompt.length() - 150);
+            extremePrompt = new StringBuilder(extremePrompt.substring(randomLoc));
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+        return extremePrompt.toString();
     }
 }
