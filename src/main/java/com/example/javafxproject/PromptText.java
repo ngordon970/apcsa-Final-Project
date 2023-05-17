e22cb265d89f53d97645b455aefb687c26f2b94f
ngordon970 <133019853+ngordon970@users.noreply.github.com>
2023-05-17 08:47:08 -0400

    Update PromptText.java

--- src/main/java/com/example/javafxproject/PromptText.java
+++ src/main/java/com/example/javafxproject/PromptText.java
@@ -3,6 +3,7 @@ import java.io.FileReader;
 
 
 public class PromptText {
+    
     public static String generateEasyPrompt() {
         StringBuilder easyPrompt = new StringBuilder();
         try {
@@ -21,4 +22,24 @@ public class PromptText {
         }
         return easyPrompt.toString();
     }
+    
+        public static String generateMediumPrompt() {
+        StringBuilder medPrompt = new StringBuilder();
+        try {
+            FileReader reader = new FileReader("C:\\Users\\Noah\\IdeaProjects\\javaFXPROJECT\\src\\main\\java\\com\\example\\javafxproject\\mediumPrompt.txt");
+            int data = reader.read();
+            while(data != -1) {
+                medPrompt.append((char)data);
+                data = reader.read();
+            }
+            reader.close();
+            int randomLoc = (int)(Math.random() * medPrompt.length() - 150);
+            medPrompt = new StringBuilder(medPrompt.substring(randomLoc));
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+        return medPrompt.toString();
+    }
+    
 }
