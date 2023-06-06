b742f9cfbf3a83f93e58aaff7ba9045e8a49d13e
ngordon970 <133019853+ngordon970@users.noreply.github.com>
2023-06-06 18:15:10 -0400

    Update Calculations.java

--- src/main/java/com/example/javafxproject/Calculations.java
+++ src/main/java/com/example/javafxproject/Calculations.java
@@ -3,9 +3,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Calculations {
-
-    private static ArrayList<Character> missedChars = new ArrayList<>();
-
     public static double getWPM(int timeElapsed) {
 
         ArrayList<String> words = new ArrayList<>(List.of(SceneController.getUserContent().split(" ")));
@@ -22,22 +19,17 @@ public class Calculations {
         int charsCorrect = 0;
 
         for (int i = 0; i < userInput.length(); i++) {
-        boolean match = false;
+
             for (int j = i; j < passage.length(); j++) {
                 if (!alreadyMatched[j] && userInput.substring(i, i + 1).equals(passage.substring(j, j + 1))) {
                     alreadyMatched[j] = true;
                     charsCorrect++;
-                    match = true;
                     break;
                 }
-            }
-            if(!match) {
-                missedChars.add(userInput.charAt(i));
             }
         }
         return ((double) charsCorrect / userInput.length()) * 100;
     }
-    public void clearCharList() {
-        missedChars.clear();
-    }
+
+    public int getCurrentPromptRow(int currIndex) { return currIndex / 66 + 1; }
 }
