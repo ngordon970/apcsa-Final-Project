ef9804f20dc9e7403f193108c385dc649ff45df6
ngordon970 <133019853+ngordon970@users.noreply.github.com>
2023-06-07 10:10:14 -0400

    Update Main.java

--- src/main/java/com/example/javafxproject/Main.java
+++ src/main/java/com/example/javafxproject/Main.java
@@ -2,7 +2,6 @@ package com.example.javafxproject;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import java.io.File;
@@ -13,10 +12,9 @@ public class Main extends Application {
     }
     @Override
     public void start(Stage stage) throws Exception {
-        URL url = new File("C:\\Users\\Noah\\IdeaProjects\\javaFXPROJECT\\src\\main\\java\\com\\example\\javafxproject\\scene1.fxml").toURI().toURL();
+        URL url = new File("C:\\Users\\24gordonno\\IdeaProjects\\apcsa-Final-Project\\src\\main\\java\\com\\example\\javafxproject\\scene1.fxml").toURI().toURL();
         Parent root = FXMLLoader.load(url);
         Scene scene = new Scene(root);
-        stage.getIcons().add(new Image("C:\\Users\\Noah\\IdeaProjects\\javaFXPROJECT\\src\\main\\java\\com\\example\\javafxproject\\TURBOcopy.png"));
         stage.setResizable(false);
         stage.setTitle("TurboType");
         stage.setScene(scene);
